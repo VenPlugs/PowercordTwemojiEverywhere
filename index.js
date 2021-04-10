4a3bc209526217a329faf68085c204860d843d00
Vendicated <vendicated@riseup.net>
2021-04-10 04:30:00 +0200

    Was never a good idea

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const twemoji = require("./twemoji.min.js");
 
 module.exports = class TwemojiEverywhere extends Plugin {
   async startPlugin() {
-    this.observer = new MutationObserver(record => {
+    this.observer = new MutationObserver((record) => {
       if (record[0].target.classList === null) return;
       this.init();
     });
@@ -13,14 +13,12 @@ module.exports = class TwemojiEverywhere extends Plugin {
     });
     this.loadStylesheet("style.css");
   }
-  
+
   pluginWillUnload() {
     if (this.observer) this.observer.disconnect();
   }
 
   init() {
-    if (typeof this.timeout !== "undefined") return;
-    this.timeout = setTimeout(() => delete this.timeout, 1000);
     // this.log("Detected change, reapplying twemoji");
     twemoji.parse(document.body);
   }
